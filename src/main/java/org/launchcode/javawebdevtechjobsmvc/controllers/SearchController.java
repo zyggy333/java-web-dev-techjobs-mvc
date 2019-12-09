797460561058611739e5c3dc17ee065c68699028
Zyggy Buss <zyggy333@gmail.com>
2019-12-09 13:32:56 -0600

    All finished

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,23 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+    @RequestMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs;
+        model.addAttribute("columns", columnChoices);
+        if (searchType.toLowerCase().equals("all")) {
+            if(searchTerm.toLowerCase().equals("")){
+                jobs = JobData.findAll();
+                model.addAttribute("title", "Jobs With All: ");
+            }else{
+                jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+                model.addAttribute("title", "Jobs With All: " + searchTerm);
+            }
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
 }
